475869a555b589d54c48ac58b22be86eb3be32ce
Himali Malvawala <himalimalvawala@gmail.com>
2023-01-18 22:17:59 +0530

    backend changes

--- pages/api/album/[album_id]/songs/[song_id].ts
+++ pages/api/album/[album_id]/songs/[song_id].ts
@@ -5,21 +5,21 @@ export default async function songDetails(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const songID = parseInt(req.query.song_id as string);
-  const { name, image } = JSON.parse(req.body);
-
   if (req.method === "GET") {
+    const songID = parseInt(req.query.song_id as string);
     try {
       const response = await prisma.songs.findUnique({
         where: { id: songID },
       });
-      res.status(200).json({ response });
+      res.status(200).json(response);
     } catch (error) {
       res.status(404).json({ message: "Couldn't find the mentioned song" });
     }
   }
 
   if (req.method === "PUT") {
+    const songID = parseInt(req.query.song_id as string);
+    const { name, image } = req?.body;
     try {
       const response = await prisma.songs.update({
         where: { id: songID },
@@ -28,13 +28,14 @@ export default async function songDetails(
           image,
         },
       });
-      res.status(200).json({ response });
+      res.status(200).json(response);
     } catch (error) {
       res.status(404).json({ message: "Couldn't update the mentioned song" });
     }
   }
 
   if (req.method === "DELETE") {
+    const songID = parseInt(req.query.song_id as string);
     try {
       const response = await prisma.songs.delete({
         where: { id: songID },
