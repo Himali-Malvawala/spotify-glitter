554a653a83c2af2d4e54c1c6bc702e4c6292fcb2
Himali Malvawala <himalimalvawala@gmail.com>
2023-01-24 20:08:26 +0530

    change the client to official docs one

--- utils/client.ts
+++ utils/client.ts
@@ -1,25 +1,40 @@
-import { PrismaClient } from "@prisma/client";
+//FOR THE LOCAL, IT WORKED ALL THE TIME
+// import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+//FROM THE PRISMA DEPLOY-TO-VERCEL BLOG
+// import { PrismaClient } from '@prisma/client'
 
-// let prisma: PrismaClient;
+// // Avoid instantiating too many instances of Prisma in development
+// // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
+// let prisma
 
-// if(process.env.NODE_ENV === 'production'){
-//     prisma = new PrismaClient();
-// }else{
-//     if(!global.prisma){
-
-//         global.prisma = new PrismaClient()
-
-//     }
-
-//     prisma = global.prisma;
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
 // }
 
 // export default prisma;
+
+//FROM PRISMA OFFICAL DOCS
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
